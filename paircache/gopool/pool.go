3d132145f55da603e95aa4144eb9fd535d2a3cca
songwen <songwen276@163.com>
2024-11-01 15:36:01 +0800

    修改roi结果插入结果通道

---
+++ paircache/gopool/pool.go
@@ -0,0 +1,22 @@
+package pairpool
+
+import (
+	"github.com/panjf2000/ants/v2"
+	"time"
+)
+
+var (
+	// Init a instance pool when importing ants.
+	defaultPool, _ = ants.NewPool(10, ants.WithExpiryDuration(10*time.Second))
+)
+
+// Logger is used for logging formatted messages.
+type Logger interface {
+	// Printf must have the same semantics as log.Printf.
+	Printf(format string, args ...interface{})
+}
+
+// Submit submits a task to pool.
+func Submit(task func()) error {
+	return defaultPool.Submit(task)
+}
