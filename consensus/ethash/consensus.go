c385bc643802e53af9654e53ce6d535b68643a52
DengYongfu <694668129@qq.com>
2024-11-19 11:39:21 +0800

    修改GetNextValidators逻辑

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -494,6 +494,11 @@ func (ethash *Ethash) NextInTurnValidator(chain consensus.ChainHeaderReader, hea
 	return common.Address{}, errors.New("not implemented")
 }
 
+// NextNextInTurnValidator return the next and next in-turn validator for header
+func (ethash *Ethash) NextNextInTurnValidator(chain consensus.ChainHeaderReader, header *types.Header) (common.Address, error) {
+	return common.Address{}, errors.New("not implemented")
+}
+
 // Prepare implements consensus.Engine, initializing the difficulty field of a
 // header to conform to the ethash protocol. The changes are done inline.
 func (ethash *Ethash) Prepare(chain consensus.ChainHeaderReader, header *types.Header) error {
